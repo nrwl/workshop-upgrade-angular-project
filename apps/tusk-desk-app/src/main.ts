9b8908a26ba309d7f45ef9fa5466ecbba7b01eea
Justin Schwartzenberger <schwarty@gmail.com>
2017-10-23 12:17:49 -0700

    add tuskdeskjs app code

--- apps/tusk-desk-app/src/main.ts
+++ apps/tusk-desk-app/src/main.ts
@@ -2,11 +2,27 @@ import { enableProdMode, StaticProvider } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import * as angular from 'angular';
+import 'angular-route';
 import { downgradeComponent, downgradeModule, setAngularLib } from '@angular/upgrade/static';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import 'tusk-desk-app';
+import './assets/app.module.js';
+import './assets/app.config.js';
+import './assets/tickets/tickets.module.js';
+import './assets/tickets/ticket-overview/ticket-overview.component.js';
+import './assets/tickets/ticket-list/ticket-list.component.js';
+import './assets/tickets/ticket-card/ticket-card.component.js';
+import './assets/tickets/ticket-activity/ticket-activity.component.js';
+import './assets/tickets/ticket.service.js';
+import './assets/tickets/user.service.js';
+import './assets/companies/companies.module.js';
+import './assets/companies/company-overview/company-overview.component.js';
+import './assets/companies/company-list/company-list.component.js';
+import './assets/companies/company-card/company-card.component.js';
+import './assets/companies/company-activity/company-activity.component.js';
+import './assets/companies/company.service.js';
+import './assets/companies/team.service.js';
 import { AppComponent } from './app/app.component';
 
 export function bootstrapAngular(extra: StaticProvider[]): any {
